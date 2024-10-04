2f6bb178151a2be70f1a10556a7bac46a685e3c9
Isabelle Costa <isabellecosta.digitalco@gmail.com>
2024-10-04 00:33:05 -0300

    Ajeitando

---
+++ essencial/frontend/src/data/hooks/useProdutos.ts
@@ -0,0 +1,30 @@
+'use client'
+import { Produto } from "@/core";
+import { useEffect, useState } from "react";
+
+const urlBase = 'http://localhost:3005'
+
+export default function useProdutos() {
+    const [produtos, setProdutos] = useState<Produto[]>([])
+
+    async function obterProdutos(): Promise<Produto[]> {
+        const resp = await fetch(`${urlBase}/produtos`)
+        const produtos = await resp.json()
+        return produtos ?? []
+    }
+
+    async function obterProdutosPorId(id: number): Promise<Produto | null> {
+        const resp = await fetch(`${urlBase}/produtos/${id}`)
+        const produto = await resp.json()
+        return produto ?? null
+    }
+
+    useEffect(() => {
+        obterProdutos().then(setProdutos)
+    }, [])
+
+    return {
+        produtos,
+        obterProdutosPorId,
+    }
+}
